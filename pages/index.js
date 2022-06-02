5551d8252b6e21650b161cfa2b03d61c98654044
buttonmashing <taoaustin@yahoo.ca>
2022-06-02 14:11:17 -0400

    test

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <>
       <Box borderRadius="lg" bg="gray" p={3} mb={6} align="center">
-        Hi, I&apos;m Austin. I&apos;m a software developer.
+        Hi, I&apos;m Austin. I&apos;m a software developer. This is a test
       </Box>
 
       <Flex direction="column">
@@ -13,39 +13,6 @@ const HomePage = () => {
         </Heading>
         <p>
           Student / Developer
-          <br></br>
-          <br></br>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
         </p>
       </Flex>
     </>
